7d91833cb1eface5798bc3947e4d83fa4f788845
nxbodyevzncvre <denielmaestro@gmail.com>
2023-02-13 21:50:09 +0500

    add some photos

---
+++ script.js
@@ -0,0 +1,18 @@
+function returnNeighboringNumbers(num) {
+	let arr = [num - 1, num, num + 1];
+	return arr;
+}
+
+console.log(returnNeighboringNumbers(15));
+
+
+
+function getMathResult(num, goal) {
+	if (typeof goal === "string" || goal <= 0 ){
+		return num
+	}else{
+		
+	}
+
+
+}
