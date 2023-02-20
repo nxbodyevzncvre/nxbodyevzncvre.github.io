98cdd9e033c7a0a9fc79a2372ca910cde00d4662
nxbodyevzncvre <denielmaestro@gmail.com>
2023-02-20 22:25:22 +0500

    refactoring

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-function returnNeighboringNumbers(num) {
-	let arr = [num - 1, num, num + 1];
-	return arr;
+function redirect() {
+	window.location.href = "/egor/egor.html";
+};
+function instRedirectEgor(){
+	window.location.href = 'https://www.instagram.com/flrbi_es/'
+};
+function instRedirectEdyge() {
+	window.location.href = 'https://www.instagram.com/kazi_ed/'
+};
+function instRedirectArtem() {
+	window.location.href = 'https://www.instagram.com/gil.tema/'
+};
+function instRedirectDen4ik() {
+	window.location.href = 'https://www.instagram.com/nxbodyevzncvre/'
+};
+function instRedirectBaxa() {
+	window.location.href = 'https://www.instagram.com/qiqdnow'
+};
+function instRedirectKhaled() {
+	window.location.href = 'https://www.instagram.com/tourablo'
+};
+function instRedirectEra() {
+	window.location.href = 'https://www.instagram.com/ersultanoooo/'
+};
+function instRedirectBoris() {
+	window.location.href = 'https://www.instagram.com/cocojohny8/'
+};
+function instRedirectShlak() {
+	window.location.href = 'https://ru.wikipedia.org/wiki/%D0%A8%D0%BB%D0%B0%D0%BA';
+};
+
+function aqtobeRedirection() {
+	window.location.href = 'https://ru.wikipedia.org/wiki/%D0%90%D0%BA%D1%82%D0%BE%D0%B1%D0%B5';
+};
+
+function almatyRedirection() {
+	window.location.href = 'https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%BC%D0%B0-%D0%90%D1%82%D0%B0';
+};
+
+function ustKamenogorsRedirection() {
+	window.location.href = 'https://ru.wikipedia.org/wiki/%D0%A3%D1%81%D1%82%D1%8C-%D0%9A%D0%B0%D0%BC%D0%B5%D0%BD%D0%BE%D0%B3%D0%BE%D1%80%D1%81%D0%BA';
+};
+function karagandaRedirection() {
+	window.location.href = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%80%D0%B0%D0%B3%D0%B0%D0%BD%D0%B4%D0%B0';
+};
+function shymkentRedirection() {
+	window.location.href =
+		'https://ru.wikipedia.org/wiki/%D0%A8%D1%8B%D0%BC%D0%BA%D0%B5%D0%BD%D1%82'
+};
+
+function connect() {
+	window.location.href = 'https://vk.com/nxbodyevzncvre'
 }
-
-console.log(returnNeighboringNumbers(15));
-
-
-
-function getMathResult(num, goal) {
-	if (typeof goal === "string" || goal <= 0 ){
-		return num
-	}else{
-		
-	}
-
-
+function connectBaha() {
+	window.location.href = 'https://vk.com/bolatn5e'
 }
